79c7572224d5105f18d1dc54a286c2ae149c18e4
Karoly Hornyak <karoly.webdev@gmail.com>
2025-03-29 13:30:43 +0000

    Add project-1

---
+++ project-1/band-name-generator.py
@@ -0,0 +1,9 @@
+print("🎸 Welcome to the Ultimate Band Name Generator! 🎶")
+
+# Getting user inputs
+city = input("🌆 What's the name of the city you grew up in? ").strip().title()
+pet_name = input("🐾 What's your pet's name? ").strip().title()
+
+# Generating the band name
+print("\n🔮 Generating your band name...\n")
+print(f"🎤 Your epic band name could be: **{city} {pet_name}!** 🤘")

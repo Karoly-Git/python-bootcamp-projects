d1f7056c5e6f72b0ee44a766935a49a55fe7c0b3
Karoly Hornyak <karoly.webdev@gmail.com>
2025-03-30 15:46:00 +0100

    Add project-6

---
+++ project-6/Maze.py
@@ -0,0 +1,15 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# copy and paste the code to the editor is available under the URL is above
+
+def turn_right():
+    for turn in range(0, 3):
+        turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()

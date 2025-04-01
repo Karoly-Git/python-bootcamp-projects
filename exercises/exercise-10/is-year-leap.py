e81cfdb020770c0edf53c872d3243e876d390ae8
Karoly Hornyak <karoly.webdev@gmail.com>
2025-04-01 10:37:46 +0100

    Add docstring to is_leap_year function in is-year-leap.py

--- exercises/exercise-10/is-year-leap.py
+++ exercises/exercise-10/is-year-leap.py
@@ -1,4 +1,5 @@
 def is_leap_year(year):
+    """Determines whether a given year is a leap year or not."""
     return (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
 
 print(is_leap_year(1989))

bd042001ffedcb469e8be349cc16054897eccaa5
Karoly Hornyak <karoly.webdev@gmail.com>
2025-04-01 10:30:00 +0100

    Add exercise-10

---
+++ exercises/exercise-10/is-year-leap.py
@@ -0,0 +1,9 @@
+def is_leap_year(year):
+    return (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+
+print(is_leap_year(1989))
+print(is_leap_year(2400))
+
+for year in range(2025, 2100):
+    if is_leap_year(year):
+        print(f"{year} is leap")

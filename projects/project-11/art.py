1976e016a3d259632dcc8176b0dbd4b116c30918
Karoly Hornyak <karoly.webdev@gmail.com>
2025-04-01 23:30:31 +0100

    Add project-11

---
+++ projects/project-11/art.py
@@ -0,0 +1,64 @@
+logo = r"""
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+
+cards = {}
+
+# Suits and Symbols
+suits = {
+    "Hearts": "♥",
+    "Diamonds": "♦",
+    "Clubs": "♣",
+    "Spades": "♠"
+}
+
+# Numbered Cards (2-9)
+for num in range(2, 10):
+    cards[str(num)] = {}
+    for suit, symbol in suits.items():
+        cards[str(num)][suit] = rf"""        
+.------.
+|{num:<2}    |
+|      |
+|  {symbol}   |
+|    {num:>2}|
+'------'     
+"""
+
+# Ace
+cards["Ace"] = {}
+for suit, symbol in suits.items():
+    cards["Ace"][suit] = rf"""        
+.------.
+|A     |
+|  {symbol}   |
+|      |
+|     A|
+'------'     
+"""
+
+# Face Cards
+face_cards = {
+    "Jack": "J",
+    "Queen": "Q",
+    "King": "K"
+}
+
+for face, letter in face_cards.items():
+    cards[face] = {}
+    for suit, symbol in suits.items():
+        cards[face][suit] = rf"""        
+.------.
+|{letter} /\  |
+| /  \ | 
+| {symbol}  {symbol} |
+|  \/ {letter}|
+'------'     
+"""
---
+++ projects/project-11/blackjack.py
@@ -0,0 +1,12 @@
+import art
+
+cards = art.cards
+
+# over 21: bust
+# equal: draw
+# lose
+# J, Q, K = 1
+# A = 1 or 11
+# if dealer < 17, has to take one more card
+
+print(cards["7"]["Spades"], cards["Ace"]["Spades"])

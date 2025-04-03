d523660f14814c6619e513c43b4dc9bbbbf79f01
Karoly Hornyak <karoly.webdev@gmail.com>
2025-04-03 11:46:03 +0100

    make_deck and shufle_deck functions made

--- projects/project-11/blackjack.py
+++ projects/project-11/blackjack.py
@@ -1,20 +1,50 @@
 import art
+import random
 
 cards = art.cards
 
-card_values = {
-    "Hearts":[{"Ace-1": 1},{"2": 2},{"3": 3},{"4": 4},{"5": 5},{"6": 6},{"7": 7},{"8": 8},{"9": 9},{"Jack": 10},{"Queen": 10},{"King": 10}],
-    "Diamonds":[{"Ace-1": 1},{"2": 2},{"3": 3},{"4": 4},{"5": 5},{"6": 6},{"7": 7},{"8": 8},{"9": 9},{"Jack": 10},{"Queen": 10},{"King": 10}],
-    "Clubs":[{"Ace-1": 1},{"2": 2},{"3": 3},{"4": 4},{"5": 5},{"6": 6},{"7": 7},{"8": 8},{"9": 9},{"Jack": 10},{"Queen": 10},{"King": 10}],
-    "Spades":[{"Ace-1": 1},{"2": 2},{"3": 3},{"4": 4},{"5": 5},{"6": 6},{"7": 7},{"8": 8},{"9": 9},{"Jack": 10},{"Queen": 10},{"King": 10}],
-}
+card_values = [
+    {"name": "ace", "value": 1},
+    {"name": "2", "value": 2},
+    {"name": "3", "value": 3},
+    {"name": "4", "value": 4},
+    {"name": "5", "value": 5},
+    {"name": "6", "value": 6},
+    {"name": "7", "value": 7},
+    {"name": "8", "value": 8},
+    {"name": "9", "value": 9},
+    {"name": "jack", "value": 10},
+    {"name": "queen", "value": 10},
+    {"name": "king", "value": 10}
+]
 
 card_suits = {
-    "Hearts": "♥",
-    "Diamonds": "♦",
-    "Clubs": "♣",
-    "Spades": "♠"
+    "hearts": "♥",
+    "diamonds": "♦",
+    "clubs": "♣",
+    "spades": "♠"
 }
+
+
+def make_deck(card_values, card_suits):
+    # Creates a 48-card deck, excluding Jocker
+    deck = []
+    for suit in card_suits.keys():
+        for value in card_values:
+            deck.append({"name": value["name"], "value": value["value"], "suit": suit})
+    
+    return deck
+
+def shufle_deck(deck):
+    return random.sample(deck, len(deck))
+
+deck = make_deck(card_values, card_suits)
+
+shufled_deck = shufle_deck(deck)
+
+
+for card in shufled_deck:
+    print(card)
 
 # over 21: bust
 # equal: draw
@@ -23,6 +53,9 @@ card_suits = {
 # A = 1 or 11
 # if dealer < 17, has to take one more card
 
-print(cards["7"]["Spades"], cards["Ace"]["Spades"])
+#print(cards["7"]["Spades"], cards["Ace"]["Spades"])
+#input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
-input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+# make the deck
+# mix the cards in t deck
+# get random card from deck

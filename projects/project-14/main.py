0a4f8ca1068aa1de3f649ebfb78f4131de13e019
Karoly Hornyak <karoly.webdev@gmail.com>
2025-04-06 22:48:55 +0100

    Add project-14

---
+++ projects/project-14/main.py
@@ -0,0 +1,58 @@
+import random
+import os
+from art import logo
+from art import vs
+from game_data import data
+
+def choose_random_element(data):
+    return random.choice(data)
+
+def compare(a, b):
+    return a['follower_count'] > b['follower_count']
+
+def game(data):
+    score = 0
+    elem_a = choose_random_element(data)
+    elem_b = choose_random_element(data)
+
+    while elem_a['name'] == elem_b['name']:
+        elem_b = choose_random_element(data)
+
+    while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(logo)
+
+            if score != 0:
+                print(f"You're right! Current score: {score}")
+
+            print(f"Compare A: {elem_a['name']}, a {elem_a['description']}, from {elem_a['country']}")
+            print(vs)
+            print(f"Compare B: {elem_b['name']}, a {elem_b['description']}, from {elem_b['country']}")
+
+            answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+        
+            while answer not in ['a', 'b']:
+                answer = input("Wrong input! Type 'A' or 'B': ").lower()
+
+            if (answer == 'a' and compare(elem_a, elem_b)) or (answer == 'b' and not compare(elem_a, elem_b)):
+                score += 1
+                elem_a = elem_b
+                elem_b = choose_random_element(data)
+                while elem_a['name'] == elem_b['name']:
+                    elem_b = choose_random_element(data)
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(logo)
+                print(f"Sorry, that's wrong! Final score: {score}")
+                break
+
+def main():
+    while True:
+        game(data)
+        play_again = input("Play again? (y/n): ").lower()
+        if play_again != 'y':
+            print("Thanks for playing!")
+            break
+
+
+main()
